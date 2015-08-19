77fdee50fd59350f9ee6b88d65b5bda23c4d4d3c
meilan <meilan.lin@capgemini.com>
2015-08-19 14:01:42 +0200

    Init project with yeoman

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name meilansApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the meilansApp
+ */
+angular.module('meilansApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
